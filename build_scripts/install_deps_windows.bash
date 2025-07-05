317158c3e3345d51ade0b593f81dadf1598cd842
Anton Dukhovnikov <antond@wetafx.co.nz>
2025-07-05 14:25:05 +1200

    lens corrections
    Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

--- build_scripts/install_deps_windows.bash
+++ build_scripts/install_deps_windows.bash
@@ -9,7 +9,10 @@ vcpkg install \
     ceres:x64-windows \
     openimageio:x64-windows \
     nlohmann-json:x64-windows \
-    lensfun:x64-windows \
-    exiftool
+    lensfun:x64-windows
     
-which exiftool
+curl --silent --output ./exiftool.zip https://exiftool.org/exiftool-13.32_64.zip
+unzip ./exiftool.zip
+mv ./exiftool-13.32_64/exiftool\(-k\).exe ./exiftool-13.32_64/exiftool
+
+./exiftool-13.32_64/exiftool
